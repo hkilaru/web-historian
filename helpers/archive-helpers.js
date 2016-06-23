687e066fe5ff8e7cd111b27213981a6436002d70
MKS-students <mks.students@hackreactor.com>
2016-06-23 12:31:00 -0400

    can save URLs and fetch htmls

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var http = require('http-request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -9,7 +10,9 @@ var _ = require('underscore');
  * customize it in any way you wish.
  */
 
-exports.urlArray = [];
+exports.urlListArray = [];
+exports.urlArchiveObj = {};
+
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
@@ -33,8 +36,14 @@ exports.readListOfUrls = function(url){
     if(err) {
       console.log(err)
     }
-    urlArray = data.toString().split('/n');
-    exports.isUrlInList(url, urlArray);
+    exports.urlListArray = data.toString().split('\n');
+    // exports.isUrlInList(url, exports.urlListArray);
+  });
+  fs.readFile('../archives/sites/', "utf-8", function (err, data) { // data is the contents of the file
+    if(err) {
+      console.log(err)
+    }
+    console.log("sites folder", data);
   });
 };
 
@@ -50,28 +59,61 @@ exports.isUrlInList = function(url, array){
       }
     }
     if(urlFound) {
-      console.log("url found!")
+      exports.isUrlArchived(url);
     }
     else{
-      fs.appendFile(exports.paths.list, url+"\n", function (err) {
-        if (err){(console.log(err));};
-        console.log("site appended!")
-      });
-
+      exports.isUrlArchived(url);
     }
 };
 
 exports.addUrlToList = function(url){
   //write url to sites.txt file using fs.writeFile
+  fs.appendFile(exports.paths.list, url+"\n", function (err) {
+        if (err){(console.log(err));};
+        console.log("site appended!")
+  });
 
 };
 
 exports.isUrlArchived = function(url){
-  //should return a boolean
+  var archived = false;
+  if(url in exports.urlArchiveObj) { // check if url is in directory
+    console.log("checking if URL is archived");
+     res.end(exports.paths.archivedSites + "/" + url);
+  }
+  else {
+    // exports.addUrlToList(url);
+    console.log(exports.urlArchiveObj);
+    console.log("URL not found in archive;");
+  }
     //if false, call addUrlToList and pass in the url
     //if true, call downloadUrls
 };
 
-exports.downloadUrls = function(url){
-  //should res.end the html file saved in the sites folder
+exports.downloadUrls = function(urlArray){
+  console.log("urlArray received", urlArray);
+  var i = 0;
+  function getHTML(url, index) {
+     if(index < urlArray.length) {
+        console.log("urlArray in loop", urlArray[i]);
+        var url = urlArray[i];
+        http.get(url, function (err, res) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          var html = res.buffer.toString();
+          console.log("url passed into route", url);
+          fs.writeFile(exports.paths.archivedSites + "/" + url, html, function(err) {
+            if(err) {
+              return console.log(err);
+            }
+            exports.urlArchiveObj[urlArray[i]] = true;
+          });
+        });
+    i++;
+    getHTML(urlArray[i], i);
+    }
+  }
+  getHTML(urlArray[i], i);
 };
